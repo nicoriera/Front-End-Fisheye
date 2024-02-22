77f8b0146b51f1686893d709efec5b622b1e2261
nicolas riera <nicolariera@gmail.com>
2024-02-22 15:58:26 +0100

    add fetch and data for phtographer

--- fishEye/src/scripts/templates/photographer.js
+++ fishEye/src/scripts/templates/photographer.js
@@ -1,7 +1,7 @@
 function photographerTemplate(data) {
-  const { name, portrait } = data;
+  const { name, portrait, country, city, tagline, price } = data;
 
-  const picture = `./src/assets/photographers/${portrait}`;
+  const picture = `./src/assets/photographers/Photographers_ID_Photos/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
@@ -9,8 +9,17 @@ function photographerTemplate(data) {
     img.setAttribute("src", picture);
     const h2 = document.createElement("h2");
     h2.textContent = name;
+    const location = document.createElement("p");
+    location.textContent = `${country}, ${city}`;
+    const tagLine = document.createElement("p");
+    tagLine.textContent = tagline;
+    const priceElement = document.createElement("p");
+    priceElement.textContent = `${price}€/jour`;
     article.appendChild(img);
     article.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(tagLine);
+    article.appendChild(priceElement);
     return article;
   }
   return { name, picture, getUserCardDOM };
