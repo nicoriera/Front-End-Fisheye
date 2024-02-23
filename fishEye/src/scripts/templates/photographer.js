2dc834976053912dae32e2fbc0f619e9f3834953
nicolas riera <nicolariera@gmail.com>
2024-02-23 13:15:22 +0100

    add css for photographer card

--- fishEye/src/scripts/templates/photographer.js
+++ fishEye/src/scripts/templates/photographer.js
@@ -1,25 +1,36 @@
 function photographerTemplate(data) {
-  const { name, portrait, country, city, tagline, price } = data;
+  const { name, portrait, country, city, tagline, price, id } = data;
 
   const picture = `./src/assets/photographers/Photographers_ID_Photos/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
+    article.classList.add("photographer-card");
+    const link = document.createElement("a");
+    link.setAttribute("href", `./photographer.html?id=${id}`);
     const img = document.createElement("img");
     img.setAttribute("src", picture);
+    img.setAttribute("alt", name);
+    img.setAttribute("aria-label", `Image of photographer ${name}`);
     const h2 = document.createElement("h2");
     h2.textContent = name;
+    h2.classList.add("photographer-name");
     const location = document.createElement("p");
     location.textContent = `${country}, ${city}`;
+    location.classList.add("photographer-location");
     const tagLine = document.createElement("p");
     tagLine.textContent = tagline;
+    tagLine.classList.add("photographer-tagline");
     const priceElement = document.createElement("p");
     priceElement.textContent = `${price}€/jour`;
-    article.appendChild(img);
-    article.appendChild(h2);
+    priceElement.classList.add("photographer-price");
+    article.appendChild(link);
+    link.appendChild(img);
+    link.appendChild(h2);
     article.appendChild(location);
     article.appendChild(tagLine);
     article.appendChild(priceElement);
+
     return article;
   }
   return { name, picture, getUserCardDOM };
